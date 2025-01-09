4cc29f415ad01caedf95961a35a9cb026b503f81
dannsidd <danushsiddarth0509@gmail.com>
2025-01-09 15:22:59 +0530

    Day 4

---
+++ Javascript/datatypes.js
@@ -0,0 +1,43 @@
+//Primitive datatypes
+//a = "SECE"
+//console.log(a)
+//b = true
+//console.log(b)
+
+//Checking the type of variable
+//console.log(typeof(a))
+
+//Secondary datatypes
+//array = ['a', 10 , true, [20,30], "Sri Eshwar"]
+//console.log(array)
+//console.log.apply(typeof(array))
+
+//Object 
+//
+//obj = {
+//    firstname:"Sri",
+//    lastname:"Eshwar"
+//}
+//console.log(obj.firstname)
+//console.log(obj["lastname"])
+
+//obj1 = {}
+//console.log(obj1)
+
+//obj1["dept"] = "CSE"
+//obj1["sec"] = "A"
+//console.log(obj1)
+//
+
+//obj2 = new Object();
+//console.log(obj2);
+//obj2.count=70;
+//obj2.abs=1;
+//totalPresentCount=obj2.count - obj2.abs;
+//console.log("presentCount: " + totalPresentCount)
+//console.log(obj2);
+
+//Set
+set = new Set();
+console.log(set);
+console.log(typeof(set));
---
+++ Javascript/variables.js
@@ -0,0 +1,9 @@
+var a =10
+let b = 10
+const pi = 3.14
+
+console.log(a)
+console.log(b)
+console.log(pi)
+pi = 1.22
+console.log(pi)
