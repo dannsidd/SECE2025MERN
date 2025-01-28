41a67c4f823f643dbb985cca6fbcb1ee2ff34318
dannsidd <danushsiddarth0509@gmail.com>
2025-01-28 09:08:03 +0530

    Day 13

---
+++ seceBE/index.js
@@ -0,0 +1,53 @@
+const express = require("express");
+const path = require("path");
+const mdb = require("mongoose");
+const dotenv = require("dotenv");
+const Signup = require("./models/signupSchema");
+const e = require("express");
+
+const app = express();
+dotenv.config();
+app.use(express.json());
+
+mdb
+  .connect(process.env.MONGODB_URL)
+  .then(() => {
+    console.log("MongoDB Connection Successful");
+  })
+  .catch((err) => {
+    console.log("MongoDB Connection Unsuccessful", err);
+  });
+
+app.get("/", (req, res) => {
+  res.send(
+    "Welcome to Backend my friend\nYour Roller coster starts from now on\nFasten your codebase so you can catchup of what is been taught"
+  );
+});
+app.get("/static", (req, res) => {
+  res.sendFile(path.join(__dirname, "index.html"));
+});
+
+app.post("/signup", (req, res) => {
+  var { firstName, lastName, username, email, password } = req.body;
+  try {
+    const newSignup = new Signup({
+      firstName: firstName,
+      lastName: lastName,
+      username: username,
+      email: email,
+      password: password,
+    });
+    newSignup.save();
+    res.status(201).send("Signup Successful");
+  } catch (error) {
+    res.status(400).send("Signup Unsuccessful", error);
+  }
+});
+app.get('/getsignupdet',async(req,res)=>{
+  var signUpdet = await Signup.find()
+  res.status(200).json(signUpdet)
+})
+
+app.listen(3001, () => {
+  console.log("Server Started");
+});
